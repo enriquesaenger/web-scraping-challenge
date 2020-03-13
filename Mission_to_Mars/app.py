39307b9e56e8858d9dba8e861f4f5e41c202a7ea
ricksanchez <esaenger@me.com>
2020-03-13 18:42:58 -0500

    renamed the scraping python file

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,23 +1,20 @@
 # import necessary libraries
 from flask import Flask, render_template
 
-import pymongo
+#import pymongo
 
 # create instance of Flask app
 app = Flask(__name__)
 
-conn = 'mongodb://localhost:27017'
+#conn = 'mongodb://localhost:27017'
 
-client = pymongo.MongoClient(conn)
+#client = pymongo.MongoClient(conn)
 
 # create route that renders index.html template
 @app.route("/")
-def index():
+def echo():
 
-    return render_template("index.html")
-
-@app.route("/scrape")
-
+    return render_template("index.html", text="Hey, it works!")
 
 if __name__ == "__main__":
     app.run(debug=True)
